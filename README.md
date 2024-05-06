d3590c01206065523673754ec56b081b870e463d
xirreal <me@xirreal.dev>
2024-05-06 13:46:31 +0200

    Renderdoc support, readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# NSight Loader
+### actually this loads Renderdoc too but whatever
+
+This version-agnostic Fabric mod allows you to inject NSight or Renderdoc into Minecraft, allowing you to attach at any time instead of having to launch the game through the GUI either manually or by launching the launcher (wow that sucks to say).
+
+### Usage
+
+1. Install Fabric (1.18.2 or later, as long as the loader is 0.15.0 or newer)
+2. Drop the mod into your mods folder
+3. Launch the game, pick the debugger you want to use
+4. Attach to the game process via your debugger of choice
+5. Profit?
+
+### Advanced usage
+
+You can skip the dialog (useful for some dev environments) by setting the `debugging` flag to the debugger you want.
+Allowed options are:
+```shell
+-Ddebugger=nsight
+-Ddebugger=renderdoc
+```
--- src/main/java/dev/xirreal/Main.java
+++ src/main/java/dev/xirreal/Main.java
@@ -11,11 +11,37 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import javax.swing.*;
+
 public class Main implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("nsight-loader");
+
+	public static String unpackResource(String resourceName) {
+		String assetPath = "/assets/nsight-loader/" + resourceName;
+		try (InputStream in = Main.class.getResourceAsStream(assetPath)) {
+			if (in == null) {
+				throw new RuntimeException("DLL not found: " + assetPath);
+			}
+			File tempFile = File.createTempFile("NGFX_Injection", ".dll");
+			tempFile.deleteOnExit();
+
+			try (FileOutputStream out = new FileOutputStream(tempFile)) {
+				byte[] buffer = new byte[1024];
+				int bytesRead;
+				while ((bytesRead = in.read(buffer)) != -1) {
+					out.write(buffer, 0, bytesRead);
+				}
+			}
+
+			return tempFile.getAbsolutePath();
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to load DLL from " + assetPath, e);
+		}
+	}
+
 	@Override
 	public void onInitialize() {
-	    String os = System.getProperty("os.name").toLowerCase();
+		String os = System.getProperty("os.name").toLowerCase();
 		if(!os.contains("win")) {
 			LOGGER.error("Unsupported OS: " + os);
 			return;
@@ -26,27 +52,61 @@ public class Main implements ModInitializer {
 			return;
 		}
 
-		String assetPath = "/assets/nsight-loader/NGFX_Injection.dll";
-		String NGFX_DLL;
-
-		try (InputStream in = Main.class.getResourceAsStream(assetPath)) {
-			if (in == null) {
-				throw new RuntimeException("DLL not found: " + assetPath);
+		int option = -1;
+		String optionString = System.getProperty("debugger");
+		if(optionString != null) {
+			if(optionString.equalsIgnoreCase("renderdoc")) {
+				option = JOptionPane.NO_OPTION;
+			} else if(optionString.equalsIgnoreCase("nsight")) {
+				option = JOptionPane.YES_OPTION;
 			}
-			File tempFile = File.createTempFile("NGFX_Injection", ".dll");
-			tempFile.deleteOnExit();
-			NGFX_DLL = tempFile.getAbsolutePath();
-
-			try (FileOutputStream out = new FileOutputStream(tempFile)) {
-				byte[] buffer = new byte[1024];
-				int bytesRead;
-				while ((bytesRead = in.read(buffer)) != -1) {
-					out.write(buffer, 0, bytesRead);
-				}
-			}
-		} catch (IOException e) {
-			throw new RuntimeException("Failed to load DLL from " + assetPath, e);
 		}
+
+		if(option == -1) {
+			System.setProperty("java.awt.headless", "false");
+
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (ReflectiveOperationException | UnsupportedLookAndFeelException ignored) {
+			}
+
+			String[] options = {"NSight", "Renderdoc"};
+
+			JFrame frame = new JFrame("Shader debugging");
+
+			frame.setUndecorated( true );
+			frame.setVisible( true );
+			frame.setLocationRelativeTo( null );
+			frame.requestFocus();
+
+			option = JOptionPane.showOptionDialog(frame, "Pick the debugger to be loaded", "Shader debugging", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+					options, null);
+
+			frame.dispose();
+
+			System.setProperty("java.awt.headless", "true"); // In case not having headless mode breaks something down the line...
+		}
+
+		if (option == JOptionPane.CLOSED_OPTION) {
+			LOGGER.info("Skipping injection...");
+			return;
+		}
+
+		if(option != JOptionPane.OK_OPTION) {
+			LOGGER.info("Loading Renderdoc injector...");
+			String RENDERDOC_DLL = unpackResource("renderdoc.dll");
+			try {
+				System.load(RENDERDOC_DLL);
+				LOGGER.info("Renderdoc loaded successfully");
+			} catch (UnsatisfiedLinkError e) {
+				LOGGER.error("Failed to load Renderdoc: ", e);
+			}
+			return;
+		}
+
+		LOGGER.info("Loading NSight...");
+
+		String NGFX_DLL = unpackResource("NGFX_Injection.dll");
 
 		try {
 			NGFX ngfx = new NGFX(NGFX_DLL);
@@ -67,7 +127,7 @@ public class Main implements ModInitializer {
 				LOGGER.error("No installations found");
 				return;
 			}
-			LOGGER.info("Found installation: " + newestInstallation.installationPath);
+			LOGGER.info("Found installation on " + newestInstallation.installationPath + ": drive");
 
 			List<Activity> activities = ngfx.EnumerateActivities(newestInstallation);
 			Activity activity = activities.stream().filter(a -> a.type == Activity.ActivityType.NGFX_INJECTION_ACTIVITY_FRAME_DEBUGGER).findFirst().orElse(null);
